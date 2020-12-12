32669f1e00bd3901017655a33322787f6e98afb6
teddybear0430 <teddybear0430@hotmail.com>
2020-12-12 22:17:48 +0900

    Initial commit

---
+++ pages/about/2.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+
+const About2 = () => (
+  <Layout title="About2">
+    <h1>Hello Next.js 👋</h1>
+    <p>About2</p>
+  </Layout>
+)
+
+export default About2;
---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+
+const About = () => (
+  <Layout title="About">
+    <h1>Hello Next.js 👋</h1>
+    <p>About</p>
+  </Layout>
+)
+
+export default About;
